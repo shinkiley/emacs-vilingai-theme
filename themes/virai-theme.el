910c68e9167bd2c0877937e4083eec574175ce2b
Shinki Leyblaiz <shinki.leyblaiz@yandex.ru>
2018-01-21 18:38:23 +0300

    Base commit.

---
+++ themes/virai-theme.el
@@ -0,0 +1,45 @@
+;; virai-theme.el --- 
+;;
+;; Author: Minae Yui <minae.yui.sain@gmail.com>
+;; Version: 0.1
+;; URL: 
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;; .
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+(let* ((white '("#FFFFFF" "#FFFFFF" "white"))
+       (black '("#000000" "#000000" "black"))
+       (bg black)
+       (fg white))
+  (deftheme virai "Virai theme")
+  (custom-theme-set-faces 'virai)
+  (provide-theme 'virai))
+
+
+
+;;; virai-theme.el ends here
